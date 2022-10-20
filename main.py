c891c9b546dea2f119cde64c77896542bc8eb8de
Bruno Henrique <102703745+Brunohenlopes@users.noreply.github.com>
2022-10-20 00:48:40 +0000

    primeiros codigos

--- main.py
+++ main.py
@@ -2,6 +2,16 @@
 #
 # print() = comando de saida 
 print("Hello world!")
+# Quando quiser guardar uma String!(frase)
+nome = "Bruno Henrique"
+idade = 21
+print(nome)
+print(idade)
+
+
+
+
+
 
 '''
 Comentario em bloco
