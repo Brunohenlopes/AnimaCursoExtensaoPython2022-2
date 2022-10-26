4e597c3399b9dd41c97f9d788057cd8910b961d6
Bruno Henrique <102703745+Brunohenlopes@users.noreply.github.com>
2022-10-26 23:09:48 +0000

    Aula 2

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+#Aula 1
 # Meu primeiro projeto Python!!
 #
 # print() = comando de saida 
-print("Hello world!")
+#print("Hello world!")
 # Quando quiser guardar uma String!(frase)
-nome = "Bruno Henrique"
-idade = 21
-print(nome)
-print(idade)
+#nome = "Bruno Henrique"
+#idade = 21
+#print(nome)
+#print("minha idade é " +str(idade))
+#Aula 2
+nome = input("Digite seu nome: ")
+print(f"Boa noite {nome}")
+idade = int(input("Digite sua idade:"))
+print("Sua idade é {}" .format (idade))
+soma = idade*2
+print("idade ao dobro é: {}".format(soma))
 
 
 
