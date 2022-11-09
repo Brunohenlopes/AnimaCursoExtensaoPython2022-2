1894651f51206515519938ca1fdb8ca08ea3d742
Bruno Henrique <102703745+Brunohenlopes@users.noreply.github.com>
2022-11-09 23:21:23 +0000

    Aula 3

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 #Aula 1
-# Meu primeiro projeto Python!!
-#
-# print() = comando de saida 
-#print("Hello world!")
-# Quando quiser guardar uma String!(frase)
-#nome = "Bruno Henrique"
-#idade = 21
-#print(nome)
-#print("minha idade é " +str(idade))
+#Meu primeiro projeto Python!!
+
+'''print() = comando de saida 
+print("Hello world!")
+Quando quiser guardar uma String!(frase)
+nome = "Bruno Henrique"
+idade = 21
+print(nome)
+print("minha idade é " +str(idade))'''
+
 #Aula 2
-nome = input("Digite seu nome: ")
+'''nome = input("Digite seu nome: ")
 print(f"Boa noite {nome}")
 idade = int(input("Digite sua idade:"))
 print("Sua idade é {}" .format (idade))
@@ -25,8 +26,35 @@ genero = input("Informe seu gênero: \nM:Masculino e F=Femnino: ")
 if idade >= 18 and genero == "M":
   print("Você precisa prestar serviço militar obrigatório")
 else:
-  print("Seu gênero é feminino")
+  print("Seu gênero é feminino")'''
+
+'''nome = input("Informe seu nome: ")
+nota = float(input("Digite sua nota: "))
+if (nota==10):
+  print(f"{nome} você é o bichão mesmo ")
+elif (nota < 10 and nota >= 6):
+  print("Você ficou na média")
+else:
+  print("Sua nota foi abaixo da média")'''
+# Aula 3
+
+'''contador = 1
+while(contador <= 10):
+  print(contador)
+  contador = contador + 1'''
+
+frutas = ["morango","laranja","maçã","manga"]
+#print(frutas[2])
+#print(len(frutas))
+#frutas.append("manga")
+i = 0
+while (i<4):    
+  print(frutas[i])
+  i = i + 1
   
+
+  
+
  
  
 
